c78d0c0f982ab42a02d4c6f0f8091d65f7f4e39c
Lucas Oliva <llucasoliva@gmail.com>
2022-12-13 13:31:02 -0300

    Alteração API

--- perfil/app.js
+++ perfil/app.js
@@ -6,15 +6,25 @@ mode: 'cors',
 cache: 'default'
 }
 
+const showData = (result) => {
+    for (const campo in result){
+        if(document.getElementById("usuario"+campo)){
+            document.getElementById("usuario"+campo).value = result[campo];
+        }
+    }
+}
+
 usuario.addEventListener("blur", (e) => {
     console.log(e);
     let search = usuario.value.replace(" ", "%");
     
-    fetch(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${search}?api_key=RGAPI-6ad1a17e-306a-486e-895a-5e409151ff6d`, options)
+    fetch(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${search}?api_key=RGAPI-f38bac64-ea5f-4a62-bc10-197b1e1faa88`, options)
     .then(response => response.json())
     .then(data => {
             console.log(data)
+            showData(data)
+        })
+        .catch(e => {
+            console.log("ERRO: " +e)
         })
     })
-
-
